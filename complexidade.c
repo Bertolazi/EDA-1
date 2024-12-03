d84f7c92f06879a8c84d215b3eaf7ad098f4938f
Bertolazi <gabriel.basto.bertolazi@gmail.com>
2024-12-03 17:42:32 -0300

    adicionando primeira parte conteúdo complexidade

---
+++ complexidade.c
@@ -0,0 +1,13 @@
+/*
+Como medir algoritmos:
+Tempo real de máquina como medida:
+    . real: Tempo total para execução (contando todos os processos em execução)
+    . user: Tempo exclusivo do processo executado
+    . sys: Tempo do sistema dedicado a execução do programa
+    . Problema: São dependentes de fatores como a libguagem, hardware e/ou processos de execução
+    . Precisamos então de uma medida independente da máquina
+Contar quantas instruções foram executadas?
+    . Análise de algoritmos: Analisar a quantidade de recursos que um algoritmo precisa para resolver um problema
+    . Considerar somente as operações relevantes
+    . Observansdo a
+*/
